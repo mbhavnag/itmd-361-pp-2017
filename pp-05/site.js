af6176ef9ba9cd11e4eb382dec396e4a83efcf19
mbhavnag <mbhavnag@hawk.iit.edu>
2017-11-13 23:43:32 -0600

    Fixed errors from linters

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,18 +1,15 @@
 $.noconflict();
-
-//function
+// function
 (function($) {
   var doorbell = new Audio('media/doorbell.mp3');
   $('html').removeClass('nojs').addClass('js');
   $('audio').remove();
-  $('#doorbell').on('click', function(){
+  $('#doorbell').on('click', function() {
     doorbell.play();
   });
-
-window.addEventListener("keypress", function(e){
-  if (e.key === "d") {
-    doorbell.play();
-  }
-});
-
+  window.addEventListener("keypress", function(e) {
+    if (e.key === "d") {
+      doorbell.play();
+    }
+  });
 })(jQuery);
